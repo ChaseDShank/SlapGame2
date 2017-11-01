77460bea9bee353a9dcb364555ca87dce72a8acd
Chase Shank <216cds@gmail.com>
2017-11-01 12:09:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+newGame()
+function newGame() {
+    mhealth = 100
+    uhealth = 100
+    hits = 0
+    update()
+}
+
+
+
+// update ----------------------------------------------------------
+
+function update() {
+    document.getElementById('uhealth').innerHTML = uhealth
+    document.getElementById('mhealth').innerHTML = mhealth
+    document.getElementById('hits').innerHTML = hits
+    if (mhealth <= 0 || uhealth <=0) {
+        endGame()
+    }
+}
+
+// attack functions ---------------------------------------
+
+function slap() {
+    mhealth -= 1
+    hits++
+    update()
+}
+function spank() {
+    mhealth -= 5
+    hits++
+    update()
+}
+function spike() {
+    mhealth -= 10
+    hits++
+    update()
+}
+
+// item functions -------------------------------------------
+function banana() {
+    mhealth += 7
+    update()
+}
+
+function feces() {
+    var num = Math.random()
+    if(num >= .4)
+    mhealth -= 20
+    update()
+}
+
+function taunt() {
+    var num = Math.random()
+    if(num >= .7)
+    uhealth -= 10
+    update()
+}
+
+
+
+function endGame() {
+    mhealth = 100
+    uhealth = 100
+    hits = 0
+    alert("Nice lol, you killed the monkey.")
+}
+
+
+
+
+
